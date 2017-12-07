f495bb71e1bad590d80031eec6548252c2856345
Nematjon <nnarziev95>
2017-12-07 21:09:50 +0900

    sdvbnm,.

---
+++ app/src/main/java/com/example/nematjon/banking/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.example.nematjon.banking;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void logInClick(View view) {
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+    public void registerClick(View view) {
+    }
+}
